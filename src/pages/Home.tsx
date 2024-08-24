f2260d32ee462e0e478e2d1813d47473d1575f5b
sghosal <sghosal@test.com>
2024-08-24 20:30:54 +0530

    styles implemented, search, add barcode functionality added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import Papa from 'papaparse'
 const Home: React.FC = () => {
   const webcamRef = useRef<Webcam>(null)
   const codeReaderRef = useRef<BrowserMultiFormatReader | null>(null)
+  const searchFieldRef = useRef<HTMLInputElement>(null)
 
   const [videoDevices, setVideoDevices] = useState<MediaDeviceInfo[]>([])
   const [selectedDevice, setSelectedDevice] = useState<MediaDeviceInfo | null>(null)
@@ -17,6 +18,9 @@ const Home: React.FC = () => {
   const [csvData, setCsvData] = useState<string[][]>([])
   const [fileName, setFileName] = useState('')
   const [newField, setNewField] = useState('')
+  const [newFieldAdded, setNewFieldAdded] = useState(false)
+  const [searchQuery, setSearchQuery] = useState('')
+  const [selectedRow, setSelectedRow] = useState(-1)
 
   useEffect(() => {
     const getVideoDevices = async () => {
@@ -42,11 +46,13 @@ const Home: React.FC = () => {
       codeReaderRef.current = codeReader
 
       setIsScanning(true)
+      setBarcode('')
       console.log('Scanning...');
       codeReader.decodeFromVideoDevice(selectedDevice.deviceId, webcamRef.current?.video!, (result: Result | null, err: any) => {
         if (result) {
           setBarcode(result.getText())
           stopScanning()
+          searchFieldRef.current?.focus()
           console.log("Scanned Barcode: ", result.getText())
         }
         if (err && !(err instanceof NotFoundException)) {
@@ -66,8 +72,17 @@ const Home: React.FC = () => {
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.code === "Space") {
+      const target = event.target as HTMLElement;
+      const isInputField = target.tagName === 'INPUT' || target.tagName === 'TEXTAREA' || target.isContentEditable;
+
+      if (event.code === "Space" && !isInputField) {
+        event.preventDefault()
         startScanning()
+      } else if (event.code === 's') {
+        event.preventDefault()
+        stopScanning()
+      } else if (event.code === 'Enter' && newField && barcode && selectedRow !== -1) {
+        handleAddBarcode()
       }
     }
 
@@ -75,7 +90,7 @@ const Home: React.FC = () => {
     return () => {
       window.removeEventListener("keydown", handleKeyDown)
     }
-  }, [])
+  }, [newField, barcode, selectedRow])
 
   // this function is for drag and drop file
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
@@ -103,12 +118,34 @@ const Home: React.FC = () => {
     }
   }
 
+  const filteredData = csvData.slice(1).map((row: string[], index: number) => ({ row, orgInd: index })).filter(({ row }) =>
+    row.some(cell =>
+      cell.toLocaleLowerCase().includes(searchQuery.toLowerCase())))
+
   const handleAddField = () => {
+    if (newField) {
+      const newCsvData = csvData
+      newCsvData[0] = [newField, ...csvData[0]]
+      for (let i = 1; i < csvData.length; i++) {
+        newCsvData[i] = ['', ...csvData[i]]
+      }
+
+      setCsvData(newCsvData)
+      setNewFieldAdded(true)
+    }
+  }
+
+  const handleAddBarcode = () => {
+    const newCsvData = csvData
+    newCsvData[selectedRow + 1] = [barcode, ...csvData[selectedRow + 1].slice(1)]
+    setCsvData(newCsvData)
+    setBarcode('')
+    setSelectedRow(-1)
   }
 
   return (
     <div className='w-full min-h-dvh p-10 flex justify-around bg-background text-txt'>
-      <div className="scanner w-4/12 p-10 rounded-lg shadow-lg flex flex-col gap-2 items-center bg-base">
+      <div className="scanner w-4/12 h-fit p-10 rounded-lg shadow-lg flex flex-col gap-2 items-center bg-base">
         <select
           value={selectedDevice?.deviceId || ""}
           onChange={e => setSelectedDevice(videoDevices.find(camera => camera.deviceId === e.target.value) || null)}
@@ -119,17 +156,20 @@ const Home: React.FC = () => {
             <option key={camera.deviceId} value={camera.deviceId}>{camera.label || `Camera ${camera.deviceId}`}</option>
           )}
         </select>
-        <div className='w-full p-2 flex'>
+        <div className='w-full'>
           {
             !isScanning ?
-              <p>Press Space bar to start Scanning...</p>
-              : <>
-                <p className='w-2/3'>Scanning...</p>
+              <div className='w-full flex justify-between'>
+                <p>Press Space bar to start Scanning...</p>
+                <button className='button' onClick={startScanning}>Scan</button>
+              </div>
+              : <div className='w-full flex justify-between'>
+                <p>Scanning...</p>
                 <button
                   onClick={stopScanning}
                   className='button'
                 >Stop</button>
-              </>
+              </div>
           }
         </div>
         {isScanning &&
@@ -140,11 +180,10 @@ const Home: React.FC = () => {
             className='w-fit rounded-lg'
           />
         }
-        <h2>Scanned Barcode:</h2>
-        <p>{barcode}</p>
+        <p className='w-full'>Scanned Barcode: {barcode}</p>
       </div>
 
-      <div className="data-section w-7/12 p-6 rounded-lg flex flex-col gap-4 shadow-lg bg-base">
+      <div className="data-section w-7/12 min-h-full p-6 rounded-lg flex flex-col gap-2 shadow-lg bg-base">
         <div className="input-data">
           {
             !fileUploaded ?
@@ -173,41 +212,90 @@ const Home: React.FC = () => {
                 </label>
               </div>
 
-              : <div>
-                <p>File: <span className='text-txt-depressed'>{fileName}</span></p>
-                <div className='flex gap-4'>
-                  <label className='flex gap-4 items-end' >
-                    <span>Column name where barcode needs to be added:</span>
-                    <input type="text" className='w-1/3 border-b border-base-2 p-1 pb-0 bg-transparent focus:ring-0 ' />
-                  </label>
-                  <button className='button'>Add</button>
-                </div>
+              : <div className='w-full flex gap-1 items-center border-b-2 border-base-2 pb-1'>
+                <img src="https://www.svgrepo.com/show/458680/file-dock.svg" alt="file" className='h-4 invert' />
+                <span>File: <span className='text-txt-depressed'>{fileName}</span></span>
               </div>
           }
         </div>
         {
           csvData.length > 0 &&
-          <div className='max-h-[600px] rounded-lg shadow-lg overflow-y-auto'>
-            <table className='w-full p-4'>
-              <thead className='sticky top-0 bg-base-2 shadow-md'>
-                <tr>
-                  {csvData[0].map((data: string, index: number) => (
-                    <th key={index} className="px-4 py-2">{data}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  csvData.slice(1).map((row: string[], index1: number) => (
-                    <tr key={index1} className='border-b border-base-2'>
-                      {row.map((data: string, index2: number) => (
-                        <td key={`${index1}-${index2}`} className='px-4 py-2'>{data}</td>
-                      ))}
-                    </tr>
-                  ))
-                }
-              </tbody>
-            </table>
+          <div className='flex flex-col gap-4'>
+            <div className='table-header w-full flex justify-between items-end'>
+              {
+                newFieldAdded ?
+                  <p>Barcode field: <span className='text-txt-depressed'>{newField}</span></p>
+                  : <div className='new-field w-fit pl-2 flex gap-4 rounded-lg bg-base-2'>
+                    <input
+                      type="text"
+                      placeholder='Barcode field name...'
+                      value={newField}
+                      onChange={(e) => setNewField(e.target.value)}
+                      className='w-1/3 border-b border-base-2 p-1 pb-0 bg-transparent focus:ring-0 '
+                    />
+                    <button
+                      className='button disabled:cursor-not-allowed'
+                      disabled={!newField}
+                      onClick={handleAddField}
+                    >Add</button>
+                  </div>
+              }
+              <div className='search-field h-8 w-fit p-2 inline-flex gap-2 items-center bg-base-2 rounded-lg'>
+                {/* <SearchIcon className={'h-full stroke-2 stroke-txt-depressed'} /> */}
+                <img src="https://www.svgrepo.com/show/532552/search-alt-2.svg" alt="search" className='h-full invert' />
+                <input
+                  type="text"
+                  ref={searchFieldRef}
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  placeholder='Search in table...'
+                  className='appearance-none bg-transparent border-none focus:ring-0 text-sm text-txt-depressed'
+                />
+                <img src="https://www.svgrepo.com/show/444605/cross.svg" alt="cross" className='h-full invert cursor-pointer' onClick={() => setSearchQuery('')} />
+              </div>
+            </div>
+            <div className='max-h-[500px] rounded-lg shadow-lg overflow-y-auto'>
+              <table className='w-full p-4 text-center'>
+                <thead className='sticky top-0 bg-base-2 shadow-md'>
+                  <tr>
+                    <th className='px-4 py-2 whitespace-nowrap'>Sno</th>
+                    {csvData[0].map((data: string, index: number) => (
+                      <th key={index} className="px-4 py-2 whitespace-nowrap">{data}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    filteredData.map(({ row, orgInd }, index1: number) => (
+                      <tr
+                        key={index1}
+                        onClick={() => setSelectedRow(orgInd)}
+                        className={`border-b border-base-2 text-sm hover:cursor-pointer ${index1 === selectedRow ? 'bg-base-3 hover:bg-base-3' : 'hover:bg-gradient-to-t hover:from-base-2 hover:to-10% hover:to-base'}`}
+                      >
+                        <td className='px-4 py-2 whitespace-nowrap'>{orgInd + 1}</td>
+                        {row.map((data: string, index2: number) => (
+                          <td key={`${index1}-${index2}`} className='w-fit px-4 py-2 whitespace-nowrap'>{data}</td>
+                        ))}
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </table>
+            </div>
+            <div className='bottom-btns ml-auto flex gap-2'>
+              <button
+                disabled={!newField || !barcode || selectedRow === -1}
+                onClick={handleAddBarcode}
+                className='button disabled:cursor-not-allowed'
+              >
+                Add to the row
+              </button>
+              <button
+                className='button bg-sky-600'
+              >
+                Save file
+              </button>
+            </div>
           </div>
         }
       </div>
